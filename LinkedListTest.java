107131f4d82c7dcd66288c504393cd4488de575e
mulunehgerie <wedigeremulu@gmail.com>
2024-05-15 19:47:39 +0300

    Add files via upload

---
+++ LinkedListTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+    @Test
+    public void testAdd() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.printList();
+    }
+
+    @Test
+    public void testRemove() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.remove(3);
+        list.printList();
+    }
+
+    @Test
+    public void testContains() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        assertTrue(list.contains(3));
+        assertFalse(list.contains(6));
+    }
+}
